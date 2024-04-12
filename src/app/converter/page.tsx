c68ee2ef75cbcc54d292e3e2e5dc721db7d47305
daminchan <tetsupo777@gmail.com>
2024-04-12 15:02:39 +0900

    ページをコメントアウトしていた為発生していたエラー修正

--- src/app/converter/page.tsx
+++ src/app/converter/page.tsx
@@ -1,24 +1,14 @@
-// 'use client';
-// import React from 'react';
-// import { Box } from '@chakra-ui/react';
-// import ConversionComponents from '../../features/converter/components/ConversionComponents/ConversionComponents';
-// import { UIStateProvider } from '@/features/converter/hooks/useUIState';
-// import Section from '@/components/section/Section';
-// import Fade from '@/components/effects/Fade';
-// import { FormStateProvider } from '@/features/converter/hooks/useFormState';
+'use client';
+import React from 'react';
+import { Box } from '@chakra-ui/react';
+import Section from '@/components/section/Section';
 
-// const page = () => {
-//   return (
-//     <Box bg="gray.50" minHeight="100vh">
-//       <Section />
-//       <Fade delay={0.33}>
-//         <UIStateProvider>
-//           <FormStateProvider>
-//             <ConversionComponents />
-//           </FormStateProvider>
-//         </UIStateProvider>
-//       </Fade>
-//     </Box>
-//   );
-// };
-// export default page;
+const page = () => {
+  return (
+    <Box bg="gray.50" minHeight="100vh">
+      <Section />
+      ここは使用していないよ！
+    </Box>
+  );
+};
+export default page;
